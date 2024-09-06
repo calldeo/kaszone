ce68eacb11ecd43749d4b0057224d27894e844ff
calldeo <deoandreas264@gmail.com>
2024-09-06 11:31:42 +0700

    update

---
+++ database/migrations/2024_09_06_042300_update_datapengeluaran_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+         Schema::table('datapengeluaran', function (Blueprint $table) {
+            //
+                $table->integer('jumlah_satuan')->after('date');
+                $table->decimal('nominal',15,2)->after('jumlah_satuan');
+                $table->decimal('dll', 15,2)->after('nominal');
+                $table->string('image', 15,2)->after('dll')->nullable();
+
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
