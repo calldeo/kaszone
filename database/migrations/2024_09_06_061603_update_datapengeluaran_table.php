6d5790ec132a0db06937b5655af1618376312df1
calldeo <deoandreas264@gmail.com>
2024-09-12 09:51:19 +0700

    seed

--- database/migrations/2024_09_06_061603_update_datapengeluaran_table.php
+++ database/migrations/2024_09_06_061603_update_datapengeluaran_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         //
          Schema::table('datapengeluaran', function (Blueprint $table) {
             //
-                $table->integer('jumlah_satuan')->after('date');
+                $table->integer('jumlah_satuan')->after('jumlah');
                 $table->decimal('nominal',15,2)->after('jumlah_satuan');
                 $table->decimal('dll', 15,2)->after('nominal');
                 $table->string('image')->after('dll')->nullable();
